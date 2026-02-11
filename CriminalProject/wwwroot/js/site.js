01f176298a45f555816699fad9cbf62e90fefce1
NelaniMonde <73121180+NelaniMonde@users.noreply.github.com>
2026-02-11 11:58:10 +0200

    responsive editing and responsive nav
    completed login, registration page and nav for index

--- CriminalProject/wwwroot/js/site.js
+++ CriminalProject/wwwroot/js/site.js
@@ -2,3 +2,42 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const toggleBtn = document.getElementById("navToggle");
+const menu = document.getElementById("navMenu");
+const indexText = document.getElementById("indexDisplayText");
+let loginBlock = document.getElementById("loginGlassCard");
+
+
+let count = 0;
+
+
+
+toggleBtn.addEventListener("click", () => {
+    menu.classList.toggle("active");
+    menu.hidden = false;
+    indexText.style.marginTop = '149px';
+
+    //if (loginBlock != null) {
+    //   // loginBlock.style.marginTop = '20%';
+    //    console.log('Found the element');
+    //}
+    //else
+    //{
+    //    console.log('Element too far ' + loginBlock);
+    //}
+
+    count += 1;
+    if (count>1)
+    {
+       
+        indexText.style.marginTop = "10px";
+       // loginBlock.style.marginTop = "5%";
+        
+        count = 0;
+    }
+    
+});
+
+
+   
+

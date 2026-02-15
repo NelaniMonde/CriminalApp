deae9bbaf98b854a9c27209dc3b117aa032114d0
NelaniMonde <73121180+NelaniMonde@users.noreply.github.com>
2026-02-15 20:10:54 +0200

    navigation bar update
    updated the navigation dropdown  for smaller screens on all the view

--- CriminalProject/wwwroot/js/site.js
+++ CriminalProject/wwwroot/js/site.js
@@ -1,37 +1,104 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+﻿
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+// elements and button variables 
 const toggleBtn = document.getElementById("navToggle");
 const menu = document.getElementById("navMenu");
 const indexText = document.getElementById("indexDisplayText");
-let loginBlock = document.getElementById("loginGlassCard");
+const suspectView = document.getElementById("addSuspectView");
+const managerView = document.getElementById("addManagerView");
+const dashboardView = document.getElementById("dashBoardView");
+const showsuspectView = document.getElementById("showsuspectsView");
+const updateCriminalRecView = document.getElementById("updateCriminalRec");
+const viewCasesView = document.getElementById("viewCases");
+const updateSuspectView = document.getElementById("updateSuspect");
 
 
 let count = 0;
 
 
-
+//nav button even listener
 toggleBtn.addEventListener("click", () => {
     menu.classList.toggle("active");
     menu.hidden = false;
-    indexText.style.marginTop = '149px';
+   
+    //element event conditions
+    if (indexText != null) {
+        indexText.style.marginTop = '149px';
+    }
+    if (suspectView != null)
+    {
+        suspectView.style.marginTop = '259px';
+    }
+    if (managerView != null)
+    {
+        managerView.style.marginTop = '259px';
+    }
 
-    //if (loginBlock != null) {
-    //   // loginBlock.style.marginTop = '20%';
-    //    console.log('Found the element');
-    //}
-    //else
-    //{
-    //    console.log('Element too far ' + loginBlock);
-    //}
+    if (dashboardView != null)
+    {
+        dashboardView.style.marginTop = '259px';
+    }
 
+    if (showsuspectView != null)
+    {
+        showsuspectView.style.marginTop = '259px';
+    }
+
+    if (updateCriminalRecView != null)
+    {
+        updateCriminalRecView.style.marginTop = '259px';
+    }
+
+    if (viewCasesView != null)
+    {
+        viewCasesView.style.marginTop = '259px';
+    }
+
+    if (updateSuspectView != null)
+    {
+        updateCriminalRecView.style.marginTop = '259px';
+    }
+
+    //doing a count for when the nav button is clicked once more
     count += 1;
+
     if (count>1)
     {
-       
-        indexText.style.marginTop = "10px";
-       // loginBlock.style.marginTop = "5%";
+        //going back to normal condition 
+        if (indexText != null)
+        {
+            indexText.style.marginTop = "10px";
+        }
+
+        if (suspectView != null)
+        {
+            suspectView.style.marginTop = '20px';
+        }
+
+        if (managerView != null) {
+            managerView.style.marginTop = '20px';
+        }
+
+        if (dashboardView != null) {
+            dashboardView.style.marginTop = '8%';
+        }
+
+        if (showsuspectView != null) {
+            showsuspectView.style.marginTop = '8%';
+        }
+
+        if (updateCriminalRecView != null) {
+            updateCriminalRecView.style.marginTop = '12%';
+        }
+
+        if (viewCasesView != null) {
+            viewCasesView.style.marginTop = '8%';
+        }
+
+        if (updateSuspectView != null) {
+            updateCriminalRecView.style.marginTop = '8%';
+        }
         
         count = 0;
     }
